9f05e54ed2b623aac999b0b74e6ca3cd5c701664
shivam8199 <100522927+shivam8199@users.noreply.github.com>
2022-07-08 17:23:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-var items=document.getElementsByClassName('list-group-item')
-console.log(items)
-console.log(items[1])
-items[1].textContent='hello 2'
-items[1].style.fontWeight='bold'
-items[2].style.backgroundColor='green'
+// var items=document.getElementsByClassName('list-group-item')
+// console.log(items)
+// console.log(items[1])
+// items[1].textContent='hello 2'
+// items[1].style.fontWeight='bold'
+// items[2].style.backgroundColor='green'
+// //gives error
+// //items.style.backgroundColor='#f4f4f4'
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold'
+// }
+var li=document.getElementsByClassName('list-group-item')
+console.log(li)
+console.log(li[1])
+li[1].textContent='hello 2'
+li[1].style.fontWeight='bold'
+li[1].style.backgroundColor='green'
 //gives error
-//items.style.backgroundColor='#f4f4f4'
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold'
+//li.style.backgroundColor='#f4f4f4'
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor='red'
 }
